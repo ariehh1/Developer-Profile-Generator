1fafdbf900402eb429e3d184a0b9a7b4a4cdba27
Ari Horowitz <ariehh@comcast.net>
2020-01-31 15:42:42 -0800

    removing node module for testing

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const util = require("util");
+const puppeteer = require("puppeteer");
+const path = require("path");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function html(data, color) {
@@ -15,6 +17,9 @@ function html(data, color) {
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>Document</title>
         <style>
+        body {
+            -webkit-print-color-adjust: exact;
+          }
             h1 {
                 background-color: ${color};
                 color: blue;
@@ -41,30 +46,34 @@ const userInput = [
   }
 ];
 
-initial();
 async function initial() {
   try {
     const { username, color } = await inquirer.prompt(userInput);
     const queryUrl = `https://api.github.com/users/${username}`;
-    const { data } = await axios.get(queryUrl);
+    const queryUrl1 = `${queryUrl}/starred`;
+    const { data: userGithubInfor } = await axios.get(queryUrl);
+    const { data: starredInfor } = await axios.get(queryUrl1);
 
-    await writeFileAsync("index.html", html(data, color), "utf8");
+    await writeFileAsync("index.html", html(userGithubInfor, color), "utf8");
 
-    console.log(data);
+    await printPDF();
+
+    console.log(userGithubInfor, starredInfor.length);
   } catch (err) {
     console.error(err);
   }
 }
 
-const puppeteer = require("puppeteer");
+initial();
 
 async function printPDF() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://blog.risingstack.com", {
+  await page.goto(`file://${path.resolve(__dirname, "index.html")}`, {
     waitUntil: "networkidle0"
   });
   const pdf = await page.pdf({ format: "A4" });
+  await writeFileAsync("resume.pdf", pdf, "binary");
 
   await browser.close();
   return pdf;
