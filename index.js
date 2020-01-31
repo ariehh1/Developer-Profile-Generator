cc5e787477c737f4d47844abf023dba0477d5f0e
Ari Horowitz <ariehh@comcast.net>
2020-01-30 21:34:43 -0800

    added puppeteer code for pdf conversion

--- index.js
+++ index.js
@@ -55,3 +55,17 @@ async function initial() {
     console.error(err);
   }
 }
+
+const puppeteer = require("puppeteer");
+
+async function printPDF() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto("https://blog.risingstack.com", {
+    waitUntil: "networkidle0"
+  });
+  const pdf = await page.pdf({ format: "A4" });
+
+  await browser.close();
+  return pdf;
+}
